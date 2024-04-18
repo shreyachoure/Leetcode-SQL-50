9a72d013eb58cbaef01b4777992dd2fdbca713ea
Shreya Choure <choureshreya295@gmail.com>
2024-04-18 11:21:05 -0700

    Question 1 solution

---
+++ Select/1757_Recyclable_and_Low_Fat_Products.sql
@@ -0,0 +1,16 @@
+-- SQL Schema
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'))
+Truncate table Products
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N')
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
+
+-- Problem Statement: 
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+-- Return the result table in any order.
+
+-- Solution 
+SELECT product_id from Products
+where low_fats = 'Y' and recyclable = 'Y';
